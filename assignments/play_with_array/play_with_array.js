200987f0395a15e5c64617db89b30cbcad380df7
chitha_srinath <chithasaireddy@gmail.com>
2021-11-30 18:35:47 +0530

    task playwitharray

--- assignments/play_with_array/play_with_array.js
+++ assignments/play_with_array/play_with_array.js
@@ -1,37 +1,59 @@
 function getEven(arr) {
-    /* 
-        Return an array containing only even numbers as a result
-        Example
-            Input: [1,4,5,77,8,90]
-            Output: [4,8,90]
+    let a = [];
+    for (let ele of arr) {
+        if (ele % 2 == 0) {
+            a.push(ele);
+        }
 
-        Write you code below
-    */
-
+    }
+    return a
 }
+/* 
+      Return an array containing only even numbers as a result
+      Example
+          Input: [1,4,5,77,8,90]
+          Output: [4,8,90]
+
+      Write you code below
+*/
+
 
 function multiplyByN(arr, n) {
-    /* 
-        Return an array whose elements are multiplied by n
-        Example
-            Input: [1,3,4,55], 3
-            Output: [3,9,13,165]
-        Write you code below
-    */
+    for (let ele of arr) {
+        ele = ele * n;
+    }
+    return arr
 }
+/* 
+      Return an array whose elements are multiplied by n
+      Example
+          Input: [1,3,4,55], 3
+          Output: [3,9,13,165]
+      Write you code below
+  */
+
 
 function removeNthElement(arr, n) {
-    /*
-        Return the array with the element present at index n removed
-        Example
-            Input: [1,3,4,6,7] 3
-            Output: [1,3,4,7]
-        Write you code below
-    */
+    let a = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (i !== n) {
+            a.push(arr[i]);
+        }
+
+    }
+    return a
 }
+/*
+      Return the array with the element present at index n removed
+      Example
+          Input: [1,3,4,6,7] 3
+          Output: [1,3,4,7]
+      Write you code below
+  */
+
 
 module.exports = {
     getEven,
     multiplyByN,
-    removeNthElement
-}
+    removeNthElement,
+};
